a8ff5d8dd51ed8546d20638e3e94be1c3af7ab82
shaikaymaan18-sketch <shaikaymaan18@gmail.com>
2026-09-23 20:52:30 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,5 +53,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.glance:glance:1.0.0")
+    implementation("androidx.glance:glance-appwidget:1.0.0")
 }
-
